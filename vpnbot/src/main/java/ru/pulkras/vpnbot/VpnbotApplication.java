72def1577c47fd7b0114d646a09dbb14024bfec0
pulkras <maligin128@gmail.com>
2023-03-23 23:58:59 +0300

    inline keyboard

--- vpnbot/src/main/java/ru/pulkras/vpnbot/VpnbotApplication.java
+++ vpnbot/src/main/java/ru/pulkras/vpnbot/VpnbotApplication.java
@@ -1,18 +1,19 @@
 package ru.pulkras.vpnbot;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@SpringBootApplication()
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class VpnbotApplication {
 
-	public static void main(String[] args) {
-		try {
-			SpringApplication.run(VpnbotApplication.class, args);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(VpnbotApplication.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- vpnbot/src/main/java/ru/pulkras/vpnbot/service/Bot.java
+++ vpnbot/src/main/java/ru/pulkras/vpnbot/service/Bot.java
@@ -2,7 +2,6 @@ package ru.pulkras.vpnbot.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
@@ -12,8 +11,12 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.pulkras.vpnbot.config.BotConfig;
+import ru.pulkras.vpnbot.model.User;
+import ru.pulkras.vpnbot.model.UserRepository;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -24,8 +27,8 @@ import java.util.List;
 @Component
 public class Bot extends TelegramLongPollingBot {
 
-//    @Autowired
-//    private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
     final BotConfig config;
 
     static final String HELPING_TEXT = "this bot was created to use vpn\n" +
@@ -33,6 +36,7 @@ public class Bot extends TelegramLongPollingBot {
             "you can press /start to get a welcome message\n" +
             "you can press /help to see this message))\n" +
             "you can press /settings to see current settings or set yours\n" +
+            "you can just press /register to sign up\n" +
             "or you can just write author to https://t.me/pulkras";
 
     public Bot(BotConfig config) {
@@ -41,6 +45,7 @@ public class Bot extends TelegramLongPollingBot {
         listOfCommands.add(new BotCommand("/start", "get a welcome message"));
         listOfCommands.add(new BotCommand("/help", "explaining how to use this bot"));
         listOfCommands.add(new BotCommand("/settings", "see settings or set your preferences"));
+        listOfCommands.add(new BotCommand("/register", "sign up"));
         try {
             this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
         } catch(TelegramApiException tae) {
@@ -65,11 +70,14 @@ public class Bot extends TelegramLongPollingBot {
             long chatId = update.getMessage().getChatId();
             switch(messageText) {
                 case "/start":
-//                    userRegistration(update.getMessage());
                     reactionToStartCommand(chatId, update.getMessage().getChat().getFirstName());
+//                    userRegistration(update.getMessage());
                     break;
                 case "/help":
                     sendMessage(chatId, HELPING_TEXT);
+                    break;
+                case "/register":
+                    register(chatId);
                     break;
                 default:
                     sendMessage(chatId, "Sorry, command was not recognized");
@@ -77,24 +85,24 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-//    private void userRegistration(Message message) {
-//        if (userRepository.findById(message.getChatId()).isEmpty()) {
-//            Long chatId = message.getChatId();
-//
-//            Chat chat = message.getChat();
-//
-//            User user = new User();
-//
-//            user.setChatId(chatId);
-//            user.setFirstName(chat.getFirstName());
-//            user.setSurname(chat.getLastName());
-//            user.setRegisteredAt(new Timestamp(System.currentTimeMillis()));
-//
-//            userRepository.save(user);
-//
-//            log.info("user saved: " + user);
-//        }
-//    }
+    private void userRegistration(Message message) {
+        if (userRepository.findById(message.getChatId()).isEmpty()) {
+            Long chatId = message.getChatId();
+
+            Chat chat = message.getChat();
+
+            User user = new User();
+
+            user.setChatId(chatId);
+            user.setFirstName(chat.getFirstName());
+            user.setSurname(chat.getLastName());
+            user.setRegisteredAt(new Timestamp(System.currentTimeMillis()));
+
+            userRepository.save(user);
+
+            log.info("user saved: " + user);
+        }
+    }
     private void reactionToStartCommand(long chatId, String name) {
 
         String answer = "Hello, " + name + " and welcome to our vpn telegram bot!\n" +
@@ -115,5 +123,34 @@ public class Bot extends TelegramLongPollingBot {
         } catch(TelegramApiException tae) {
             log.error("Error occurrred. " + tae.getMessage());
         }
+    }
+
+    private void register(long chatId) {
+        SendMessage message = new SendMessage();
+
+        message.setChatId(String.valueOf(chatId));
+
+        message.setText("Do you really want to register?");
+
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
+
+        List<InlineKeyboardButton> rowInLine = new ArrayList<>();
+
+        InlineKeyboardButton yesButton = new InlineKeyboardButton();
+        yesButton.setText("Yes");
+        yesButton.setCallbackData("YesButton");
+
+        InlineKeyboardButton noButton = new InlineKeyboardButton();
+        noButton.setText("No");
+        noButton.setCallbackData("NoButton");
+
+        rowInLine.add(yesButton);
+        rowInLine.add(noButton);
+
+        rowsInLine.add(rowInLine);
+
+        markupInline.setKeyboard(rowsInLine);
+        message.setReplyMarkup(markupInline);
     }
 }
